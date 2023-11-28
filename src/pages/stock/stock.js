cb92a6d4cb2fc54b1360df22d75d56ddf8e305a7
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-11-28 23:23:16 +0300

    one stock page is developed more

--- src/pages/stock/stock.js
+++ src/pages/stock/stock.js
@@ -4,21 +4,30 @@ import { useParams } from "react-router-dom";
 
 function Stock() {
     const { stockName } = useParams();
+    const isLogined = true;
 
     return (
-        <div className="container text-white bg-dark mb-3">
-          
+        <div className="container stock-container text-white bg-dark mb-0">
           <div className="card-body row">
-            <h5 className="card-title col-6 left">
-                <div>
-                    {stockName} ({'XXXX'})
+            <h5 className={"card-title left " + (isLogined ? "col-4" : "col-6")}>
+                <div className='row stock-title'>
+                  <div className={"col stock-name"}>
+                      {stockName}
+                  </div>
+                  <div className={"col stock-code"}>
+                    ({'XXX'})
+                  </div>
                 </div>
-                {121} 
-                <span className={3 > 0 ? 'text-success' : 'text-danger'}>
-                    {3 > 0 ? '↑' : '↓'}
-                </span>
+                <div className='row stock-price'>
+                  <span className={"col number " + (3 > 0 ? 'text-success' : 'text-danger')}>
+                      {121}
+                  </span>
+                  <span className={"col indicator " + (3 > 0 ? 'text-success' : 'text-danger')}>
+                      {3 > 0 ? '↑' : '↓'}
+                  </span>
+                </div>
             </h5>
-            <p className="card-text col-6 right">
+            <p className={"card-text right " + (isLogined ? "col-4" : "col-6")}>
               Dün: {123}<br />
               Değişim: {3}%<br />
               En Düşük: {120}<br />
@@ -26,6 +35,24 @@ function Stock() {
               Hacim Lot: {123123312}<br />
               Hacim TL: {234234234}
             </p>
+            {(isLogined) ? <div className='col-4'>
+              <div className='row own-title align-items-center'>
+                <p>Sahiplik : {'0'} Lot</p>
+              </div>
+              <div className='row'>
+                <div className='row buy-section'>
+                  <input className='col-3' type='number' min={0}></input>
+                  <p className='col-2'>Lot</p>
+                  <btn className='col-4'>Satın Al</btn>
+                </div>
+                <div className='row sell-section'>
+                  <input className='col-3' type='number' min={0}></input>
+                  <p className='col-2'>Lot</p>
+                  <btn className='col-4'>Sat</btn>
+                </div>
+              </div>
+            </div> : <div></div> } 
+            
           </div>
         </div>
       );
