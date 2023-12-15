1c0005ca9857dbd07f6b5dbee1d3fcb19bb7661e
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-12-15 22:44:18 +0300

    header is changed when logined

--- src/layouts/header/header.js
+++ src/layouts/header/header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './header.css'
 import logo from '../../assets/img/logo.png'
+import { useAuth } from '../../context/authContext';
 
 function Header() {
-    const isLogined = true;
+    const {token} = useAuth();
     return (
         <header className='container header'>
             <nav class="navbar navbar-inverse head">
@@ -14,6 +15,17 @@ function Header() {
                             <img class="app-logo" src={logo}/>
                         </a>
                     </div>
+                    {
+                    (token) 
+                    ? 
+                    <div className="user-panel text-center">
+                        <span className="name">Harun Eren Özkaya</span>
+                        <nav className="user-navigation">
+                            <Link to="/settings" className="btn btn-link">Ayarlar</Link> |
+                            <Link to="/logout" className="btn btn-link">Çıkış</Link>
+                        </nav>
+                    </div>
+                    :
                     <ul class="nav navbar-right">
                         <li className='mx-2'>
                             <Link to="/login" className="text-decoration-none text-light">Giriş</Link>
@@ -23,6 +35,8 @@ function Header() {
                             <Link to="/register" className="text-decoration-none text-light">Kaydol</Link>
                         </li>
                     </ul>
+                    }
+                    
                 </div>
             </nav>
             <nav class="navbar navbar-dark menu">
@@ -34,7 +48,7 @@ function Header() {
                     <Link to="/currency" className="navbar-text">Döviz</Link>
                     <span className="divider">|</span>
                     <Link to="/commodity" className="navbar-text">Emtia</Link>
-                    {(isLogined) ? 
+                    {(token) ? 
                     <div className='my-investment-link'>   
                         <span className="divider">|</span>
                         <Link to="/my-investments" className="navbar-text">Yatırımlarım</Link>
