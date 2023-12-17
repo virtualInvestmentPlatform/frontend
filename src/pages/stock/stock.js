d21299ca426965c95ba4fd9f6150ce932c8e07c0
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-12-17 12:29:17 +0300

    get one stock is completed.

--- src/pages/stock/stock.js
+++ src/pages/stock/stock.js
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './stock.css'
 import { useParams } from "react-router-dom";
 import { useAuth } from '../../context/authContext';
+import { getStock } from '../../service/stock'; 
 
 function Stock() {
     const {token} = useAuth();
     const { stockName } = useParams();
+    const [stock, setStock] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await (getStock(stockName));
+            if (response && response.data) {
+                setStock(response.data);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
         <div className="container stock-container text-white bg-dark mb-0">
           <div className="card-body row">
-            <h5 className={"card-title left " + (token ? "col-4" : "col-6")}>
+            <h5 className={"card-title left " + (token ? "col-4" : "col-7")}>
                 <div className='row stock-title'>
                   <div className={"col stock-name"}>
-                      {stockName}
-                  </div>
-                  <div className={"col stock-code"}>
-                    ({'XXX'})
+                      {stock.text}
                   </div>
                 </div>
+                <div className={"row stock-code"}>
+                    ({stock.code})
+                  </div>
                 <div className='row stock-price'>
-                  <span className={"col number " + (3 > 0 ? 'text-success' : 'text-danger')}>
-                      {121}
+                  <span className={"col number " + (stock.rate > 0 ? 'text-success' : 'text-danger')}>
+                      {stock.lastprice} TL
                   </span>
-                  <span className={"col indicator " + (3 > 0 ? 'text-success' : 'text-danger')}>
-                      {3 > 0 ? '↑' : '↓'}
+                  <span className={"col indicator " + (stock.rate > 0 ? 'text-success' : 'text-danger')}>
+                      {stock.rate > 0 ? '↑' : '↓'}
                   </span>
                 </div>
             </h5>
-            <p className={"card-text right " + (token ? "col-4" : "col-6")}>
-              Dün: {123}<br />
-              Değişim: {3}%<br />
-              En Düşük: {120}<br />
-              En Yüksek: {300}<br />
-              Hacim Lot: {123123312}<br />
-              Hacim TL: {234234234}
+            <p className={"card-text right " + (token ? "col-4" : "col-5")}>
+              Değişim : {stock.rate}%<br />
+              En Düşük : {stock.min} TL<br />
+              En Yüksek : {stock.max} TL<br />
+              Hacim : {stock.hacim} TL<br />
+              Son Güncelleme : {stock.time}
             </p>
             {(token) ? <div className='col-4'>
               <div className='row own-title align-items-center'>
--- src/service/stock.js
+++ src/service/stock.js
@@ -11,4 +11,23 @@ const getAllStocks = async () => {
     }
 };
 
-export { getAllStocks };
+const getStock = async (code) => {
+    try {
+        const response = await axios.get(STOCK_PATH + "/" + code);
+        if (response != null) {
+            const stockText = response.data.text;
+            const stockCode = response.data.code;
+            const codeLength = stockCode.length;
+            if (stockText.indexOf(stockCode) === 0) {
+                const remainingText = stockText.substring(codeLength);
+                const firstHalf = remainingText.slice(0, (remainingText.length / 2) + 1);
+                response.data.text = firstHalf;
+            }
+        }
+        return response;
+    } catch (error) {
+        return null;
+    }
+};
+
+export { getAllStocks , getStock};
